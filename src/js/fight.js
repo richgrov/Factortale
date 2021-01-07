9454a4e6963ba46269b1c22de639f411bb4d0843
Hydronic <38992201+HydronicDev@users.noreply.github.com>
2021-01-06 19:41:29 -0700

    Shuffle factor options

--- src/js/fight.js
+++ src/js/fight.js
@@ -12,6 +12,25 @@ let answer;
 const player = new Player();
 
 const ready = () => {
+  const shuffle = (array) => {
+    // ARRAY SHUFFLE ALGORITHM CREDIT: https://github.com/Daplie/knuth-shuffle/blob/master/index.js
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
   const box = new Box();
   box.setText('Expression blocks the way!');
 
@@ -76,7 +95,7 @@ const ready = () => {
   });
 
   const leftFactorMenu = () => {
-    return new Menu([
+    const array = shuffle([
       {
         name: 'Factor out x',
         callback: () => {
@@ -110,11 +129,13 @@ const ready = () => {
           attack.run(0);
         }
       }
-    ], () => currentMenu = factorChooseMenu);
+    ]);
+
+    return new Menu(array, () => currentMenu = factorChooseMenu);
   };
 
   const rightFactorMenu = () => {
-    return new Menu([
+    const array = shuffle([
       {
         name: 'Factor out ' + equation.correctFactors[1],
         callback: () => {
@@ -148,7 +169,9 @@ const ready = () => {
           attack.run(0);
         }
       }
-    ], () => currentMenu = factorChooseMenu);
+    ]);
+
+    return new Menu(array, () => currentMenu = factorChooseMenu);
   };
 
   const factorChooseMenu = new Menu([
