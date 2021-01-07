737c003b1e8df0250de30084bdcc5ee3132d762c
Hydronic <38992201+HydronicDev@users.noreply.github.com>
2021-01-07 09:23:30 -0700

    Move shuffle() to util

--- src/js/fight.js
+++ src/js/fight.js
@@ -13,25 +13,6 @@ let answer;
 const player = new Player();
 
 const ready = () => {
-  const shuffle = (array) => {
-    // ARRAY SHUFFLE ALGORITHM CREDIT: https://github.com/Daplie/knuth-shuffle/blob/master/index.js
-    let currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (currentIndex !== 0) {
-      // Pick a remaining element...
-      randomIndex = random(currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  }
-
   const box = new Box();
   box.setText('Expression blocks the way!');
 
